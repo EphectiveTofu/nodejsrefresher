614394116b8a6cece3781f04aa186b83bad87207
Ken <kmartinez@sjcoe.net>
2023-05-30 16:19:18 -0700

    es6 examples objs and arrays

---
+++ jsobjects/objects.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'John',
+    age: 29,
+    greet() {
+        console.log(`Hello, I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+};
+
+console.log(person);
+person.greet();
+
+const hobbies = ['Sports', 'Cooking', 'Weightlifting'];
+// for of loop
+// for(let hobby of hobbies){
+//     console.log(hobby);
+// }
+
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
