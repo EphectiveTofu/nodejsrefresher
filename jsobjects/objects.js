2bc433b221b946d0601617073df27251f38149c1
Ken <kmartinez@sjcoe.net>
2023-06-06 13:42:46 -0700

    Rehash on JS basics

--- jsobjects/objects.js
+++ jsobjects/objects.js
@@ -9,7 +9,7 @@ const person = {
 console.log(person);
 person.greet();
 
-const hobbies = ['Sports', 'Cooking', 'Weightlifting'];
+const hobbies = ['Sports', 'Cooking'];
 // for of loop
 // for(let hobby of hobbies){
 //     console.log(hobby);
@@ -17,5 +17,29 @@ const hobbies = ['Sports', 'Cooking', 'Weightlifting'];
 
 
 //refresh on arrow functions
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+
+/*
+<----------------------------------------------------->
+*/
+
+hobbies.push('Programming');
 console.log(hobbies);
+
+/*
+<----------------------------------------------------->
+*/
+
+
+// The spread operator 
+// we are not adding a new value to an array but creating a copy of that array with all the old values with the new value attached keeping the const = hobbies 
+//This is called immutability
+
+//Creates a copy of the Array 
+const copiedArray = hobbies.slice();
+console.log(copiedArray);
+
+//Put the array into the [0] position into the new array
+const copiedArrayTwo = [hobbies];
+console.log(copiedArrayTwo);
